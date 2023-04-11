787a9d13458128778e7dc64fd2b517b619aac313
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-11 12:33:49 +0530

    get data from array in js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import _ from 'lodash';
 import './style.css';
 
+const leaderboard_data = [
+  {name: 'Name', score: 100},
+  {name: 'Name', score: 30},
+  {name: 'Name', score: 25},
+  {name: 'Name', score: 87},
+  {name: 'Name', score: 57},
+  {name: 'Name', score: 10},
+]
+
+let tbody = document.querySelector('.table-body');
+let leaderboard = leaderboard_data.map(data => `
+  <tr>
+    <td scope="row">${data.name}:</td>
+    <td>${data.score}</td>
+  </tr>
+`).join('');
+
+tbody.innerHTML = leaderboard
