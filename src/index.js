1daade9d4499c71f19b08491a34e79a055b468a5
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-11 13:29:51 +0530

    solve webhit & eslint errors

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-import _ from 'lodash';
 import './style.css';
 
-const leaderboard_data = [
-  {name: 'Name', score: 100},
-  {name: 'Name', score: 30},
-  {name: 'Name', score: 25},
-  {name: 'Name', score: 87},
-  {name: 'Name', score: 57},
-  {name: 'Name', score: 10},
-]
+const leaderboardData = [
+  { name: 'Name', score: 100 },
+  { name: 'Name', score: 30 },
+  { name: 'Name', score: 25 },
+  { name: 'Name', score: 87 },
+  { name: 'Name', score: 57 },
+  { name: 'Name', score: 10 },
+];
 
-let tbody = document.querySelector('.table-body');
-let leaderboard = leaderboard_data.map(data => `
+const tbody = document.querySelector('.table-body');
+const leaderboard = leaderboardData.map((data) => `
   <tr>
     <td scope="row">${data.name}:</td>
     <td>${data.score}</td>
   </tr>
 `).join('');
 
-tbody.innerHTML = leaderboard
+tbody.innerHTML = leaderboard;
